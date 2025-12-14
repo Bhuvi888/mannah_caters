748cf8b3418f739c55375c6cf481ec9e4eecb367
Your <your_email@example.com>
2025-12-14 18:53:18 +0530

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
-import About from "./pages/About"
 import Menu from "./pages/Menu"
 import Why from "./pages/Why"
 import Contact from "./pages/Contact"
@@ -10,13 +9,10 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/why" element={<Why />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
-
-
   )
 }
 
